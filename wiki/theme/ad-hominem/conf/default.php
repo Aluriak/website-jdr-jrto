4de48e9fec4ecc2c635e043c64966653fda7b375
Lucas Bourneuf <lucas@bourneuf.net>
2023-08-25 17:25:32 +0200

    theme handling

---
+++ wiki/theme/ad-hominem/conf/default.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Configuration settings for the Ad Hominem template
+ *
+ * @author     Sascha Leib <sascha@leib.be>
+ */
+
+$conf['darkmode']		= 'allow';
+$conf['navtrail']		= 'none';
+$conf['printstyle']		= 'basic';
+$conf['homelink']		= '';
+
+$conf['cookiepos']		= 'hide';
+$conf['cookiemsg']		= '';
+$conf['cookielink']		= 'about:cookies';
---
+++ wiki/theme/ad-hominem/tpl_footer.php
@@ -0,0 +1,23 @@
+<?php ?>
+<div id="ftSiteNavR" class="ftSection">
+    <h4>Navigation rapide</h4>
+    <ul>
+        <li class="action index"><a href="/r%C3%A8gles:" title="Règles" rel="nofollow" accesskey="g">Sommaire des règles</a></li>
+        <li class="action index"><a href="/r%C3%A8gles:guide:" title="Guides" rel="nofollow" accesskey="g">Guides</a></li>
+        <li class="action index"><a href="/r%C3%A8gles:classe:" title="Classes" rel="nofollow" accesskey="l">Classes</a></li>
+        <li class="action index"><a href="/r%C3%A8gles:aptitude:" title="Aptitudes" rel="nofollow" accesskey="c">Aptitudes</a></li>
+        <li class="action index"><a href="/r%C3%A8gles:comp%C3%A9tence:" title="Compétences" rel="nofollow" accesskey="c">Compétences</a></li>
+        <li class="action index"><a href="/jdr:faq:sommaire:" title="FAQ" rel="nofollow" accesskey="c">FAQ</a></li>
+    </ul>
+</div>
+
+
+<div id="ftSiteNavC" class="ftSection">
+    <h4>Communauté</h4>
+    <ul>
+        <li class="action index"><a href="/auberge" title="Auberge" rel="nofollow" accesskey="a">L'Auberge du Bout du Monde</a></li>
+        <li class="action index"><a href="https://discord.com/invite/WkrKQGxZvQ" title="Discord" rel="nofollow" accesskey="d">Le discord</a></li>
+        <li class="action index"><a href="https://framadate.org/Z45ij1wFy3Js7yfj" title="Framadate" rel="nofollow" accesskey="f">Framadate</a></li>
+        <li class="action index"><a href="/participer" title="Participer" rel="nofollow" accesskey="f">Participer <i>autrement</i></a></li>
+    </ul>
+</div>
